4648250b75ed212ba825f0b3835f16e06c9abbb7
yanoshingo <yanoshingo119@gmail.com>
2022-04-22 13:07:00 +0900

    画面スクロール修正テスト

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -101,7 +101,7 @@
         </div>
 
         <div class="non_height spot_card_container absolute inset-x-0 top-3/5 mx-0 w-full flex justify-center">
-            <div id="non_height" class="relative spot_card_content mx-0 overflow-x-auto flex flex-col items-center w-full xl:max-w-6xl">
+            <div id="non_height" class="absolute spot_card_content mx-0 overflow-x-auto flex flex-col items-center w-full xl:max-w-6xl">
         {{-- <div class="non_height spot_card_container absolute inset-x-0 top-2/3 mx-auto mt-4">
             <div id="non_height" class="spot_card_content w-2/3 m-auto overflow-x-scroll snap-y flex flex-col items-center"> --}}
         @if(gettype($spots) == "object")
