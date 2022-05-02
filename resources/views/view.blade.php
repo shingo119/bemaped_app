c95c1726258ab1494fe4843eb82a83b2b22392d7
yanoshingo <yanoshingo119@gmail.com>
2022-05-03 00:30:24 +0900

    アナリティクステスト

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NW8NY2HKTS"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-NW8NY2HKTS');
+        </script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
