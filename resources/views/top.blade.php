2388283eb59c44caa685942d56a331e9feebb419
yanoshingo <yanoshingo119@gmail.com>
2022-05-02 22:46:54 +0900

    アナリティクス導入

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NW8NY2HKTS"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-NW8NY2HKTS');
+        </script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
 
