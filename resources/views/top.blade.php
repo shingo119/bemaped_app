f4110478e39a2be08163d0a1b59a192bbaddddbb
yanoshingo <yanoshingo119@gmail.com>
2022-05-03 17:59:14 +0900

    スナップポイント調整

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -112,7 +112,7 @@
         </div>
 
         <div id="non_pinchin" class="non_height spot_card_container absolute inset-x-0 top-3/4 mx-0 w-full flex justify-center">
-            <div id="non_height" class="absolute spot_card_content mx-0 overflow-x-auto flex items-center snap-x w-full xl:max-w-6xl">
+            <div id="non_height" class="absolute spot_card_content rounded-xl mx-0 overflow-x-auto flex items-center snap-x snap-mandatory w-full xl:max-w-6xl">
         {{-- <div class="non_height spot_card_container absolute inset-x-0 top-2/3 mx-auto mt-4">
             <div id="non_height" class="spot_card_content w-2/3 m-auto overflow-x-scroll snap-y flex flex-col items-center"> --}}
         @if(gettype($spots) == "object")
