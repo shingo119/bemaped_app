b045da70f8b327d3c34bf28ae0d3a8e7a81ae155
yanoshingo <yanoshingo119@gmail.com>
2022-05-03 14:46:10 +0900

    アナリティクス最新バージョンに修正。

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,21 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-227736458-1"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-75XFPLLNQ1"></script>
         <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'UA-227736458-1');
+        gtag('config', 'G-75XFPLLNQ1');
         </script>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TTB2563');</script>
-        <!-- End Google Tag Manager -->
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
@@ -61,10 +54,6 @@
         </style>
     </head>
     <body>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TTB2563"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
         <div id="myMap" class="w-screen"></div>
         <form method="POST" action="{{ route('search')}}">
             @csrf
