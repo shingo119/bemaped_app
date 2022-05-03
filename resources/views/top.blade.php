a477759860c45b194e663ea2854dda4a52cdf57b
yanoshingo <yanoshingo119@gmail.com>
2022-05-03 13:30:34 +0900

    タグマネージャーの追加

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -10,6 +10,13 @@
 
         gtag('config', 'UA-227736458-1');
         </script>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TTB2563');</script>
+        <!-- End Google Tag Manager -->
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
@@ -54,6 +61,10 @@
         </style>
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TTB2563"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <div id="myMap" class="w-screen"></div>
         <form method="POST" action="{{ route('search')}}">
             @csrf
--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -10,7 +10,13 @@
 
         gtag('config', 'UA-227736458-1');
         </script>
-
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TTB2563');</script>
+        <!-- End Google Tag Manager -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -55,6 +61,10 @@
         </style>
     </head>
     <body class="flex justify-start">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TTB2563"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <div class="w-full bg-gray-100">
             <div class="h-16 flex items-center justify-center"><a href="{{route('index')}}"><p class="m-0 text-3xl font-bold text-yellow-400">bemaped</p></a></div>
             <div id="hidden_veiw" class="w-full overflow-y-hidden flex justify-center">
